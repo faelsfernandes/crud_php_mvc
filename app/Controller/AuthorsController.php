9166af026a33949b9061d9720eb6515c9e2f3ab3
Rafael Fernandes <faelsfernandes@gmail.com>
2020-01-23 10:28:33 -0300

    add: modals

--- app/Controller/AuthorsController.php
+++ app/Controller/AuthorsController.php
@@ -6,7 +6,37 @@ class AuthorsController
     {
         try{
             $authors = Authors::selectAuthors();
-            echo "<button style='margin-left: 10px' type='button' class='btn btn-success'>Add an author</button><br><br>";
+
+            echo "<button style='margin-left: 10px' type='button' class='btn btn-success' data-toggle='modal' data-target='#modalAddAuthor'>Add an Author</button><br><br>";  
+            echo "</button>";
+            
+            echo "<div class='modal fade' id='modalAddAuthor' tabindex='-1' role='dialog' aria-labelledby='modalAddAuthorTitle' aria-hidden='true'>";
+            echo "<div class='modal-dialog' role='document'>";
+            echo "<div class='modal-content'>";
+            echo "<div class='modal-header'>";
+            echo "<h5 class='modal-title' id='modalAddAuthorTitle'>Add an Author</h5>";
+            echo "<button type='button' class='close' data-dismiss='modal' aria-label='Close'>";
+            echo "<span aria-hidden='true'>&times;</span>";
+            echo "</button>";
+            echo "</div>";
+            echo "<div class='modal-body'>";
+            echo "<form>";
+            echo "<div class='form-group'>";
+            echo "<label for='nameInput'>Name</label>";
+            echo "<input type='text' class='form-control' id='nameInput' placeholder='Ex.: José'>";
+            echo "<label for='lastnameInput'>Last Name</label>";
+            echo "<input type='text' class='form-control' id='lastnameInput' placeholder='Ex.: da Silva'>";
+            echo "</div>";
+            echo "</form>";
+            echo "</div>";
+            echo "<div class='modal-footer'>";
+            echo "<button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>";
+            echo "<button type='button' class='btn btn-success'>Add</button>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
+
             echo "<table class='table table-dark'>";
             echo "<thead class='thead-dark'>";
             echo "<tr>";
--- app/Controller/BooksController.php
+++ app/Controller/BooksController.php
@@ -6,7 +6,48 @@ class BooksController
     {
         try{
             $books = Books::selectBooks();
-            echo "<button style='margin-left: 10px' type='button' class='btn btn-success'>Add a book</button><br><br>";
+
+            echo "<button style='margin-left: 10px' type='button' class='btn btn-success' data-toggle='modal' data-target='#modalAdd'>Add a book</button><br><br>";  
+            echo "</button>";
+            
+            echo "<div class='modal fade' id='modalAdd' tabindex='-1' role='dialog' aria-labelledby='modalAddTitle' aria-hidden='true'>";
+            echo "<div class='modal-dialog' role='document'>";
+            echo "<div class='modal-content'>";
+            echo "<div class='modal-header'>";
+            echo "<h5 class='modal-title' id='modalAddTitle'>Add a Book</h5>";
+            echo "<button type='button' class='close' data-dismiss='modal' aria-label='Close'>";
+            echo "<span aria-hidden='true'>&times;</span>";
+            echo "</button>";
+            echo "</div>";
+            echo "<div class='modal-body'>";
+            echo "<form>";
+            echo "<div class='form-group'>";
+            echo "<label for='titleInput'>Name</label>";
+            echo "<input type='text' class='form-control' id='titleInput' placeholder='Ex.: A culpa é das estrelas'>";
+            echo "<label for='quantityInput'>Quantity</label>";
+            echo "<input type='number' min='1' class='form-control' id='quantityInput' placeholder='Ex.: 10'>";
+            echo "</div>";
+            echo "<div class='form-group'>";
+            echo "<label for='authorsSelect'>Authors</label>";
+            echo "<select multiple class='form-control' id='authorsSelect'>";
+            $authors = Authors::selectAuthors();
+            foreach($authors as $value)
+            {
+                echo "<option>".$value->name." ".$value->lastname."</option>";
+
+            }
+            echo "</select>";
+            echo "</div>";
+            echo "</form>";
+            echo "</div>";
+            echo "<div class='modal-footer'>";
+            echo "<button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>";
+            echo "<button type='button' class='btn btn-success'>Add</button>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
+
             echo "<table class='table table-dark'>";
             echo "<thead class='thead-dark'>";
             echo "<tr>";
